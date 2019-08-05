286e0f938a636b93a1aafc98e6b6341ef47ea378
kevintseng83 <40012974+kevintseng83@users.noreply.github.com>
2019-08-05 11:35:59 +0800

    Update test1.py

--- src/test1.py
+++ src/test1.py
@@ -6,7 +6,7 @@ import json
 import numpy as np
 import math
 import database_connect
-from utils.anomaly import ghost,ghost2, fill_lose
+from utils.anomaly import ghost, ghost2, fill_lost, fill_lost_finger
 from utils.judge import shoot_judge1, shoot_judge2, shoot_judge3
 #from line import openpose_output_path, video_date, video_id, new_video_path
 
@@ -15,7 +15,9 @@ video_path= '/home/data/uploads/'+ new_video_path
 video_date= new_video_date
 video_id= new_video_id
 openpose_output_path='/home/json_out/json'+ str(video_id)
+
 path = openpose_output_path
+
 files= os.listdir(path) 
 count=0
 original=[]
@@ -23,28 +25,19 @@ after_fill = []
 x=[]
 y=[]
 
-body_0_x=[] 
-body_2_x=[]
-body_3_x=[]
-body_4_x=[]
-body_6_x=[]  
-body_10_x=[]  #rknee
-body_13_x=[]
-body_20_x=[]
-body_23_x=[]
+body= list()
+for i in range (0, 25):
+    body.append(dict())
+    body[i]["x"]= []
+    body[i]["y"]= []
 
-body_0_y=[] 
-body_2_y=[]
-body_3_y=[]
-body_4_y=[]
-body_6_y=[]  
-body_10_y=[]  #rknee
-body_13_y=[]  #lknee
-
-"""中指尖"""
-finger_12_x=[]
-finger_12_y=[]
-
+  
+finger= list()
+for i in range (0, 20):
+    finger.append(dict())
+    finger[i]["x"]= []
+    finger[i]["y"]= []    
+    
 hand_angle=[]
 
 frame_count= []
@@ -70,6 +63,7 @@ judge_comment_3= ''
 
 json_pre1=[] #前一個
 json_pre2=[] #前兩個
+
 
  #找手最高點的圖
 def hand_high(json, num):
@@ -104,9 +98,9 @@ def elbow_shoulder(shoulder, elbow, num) :
 def judge_3(right, left, count):
     for i in range(0, count):
         if right[i]< left[i]:
-           judge_3_flag= False
+            judge_3_flag= False
 #print(judge_3_flag)        
-            
+"""            
 def angle_plot(count) :
     for i in range(0, count):
         v32_x= body_2_x[i]- body_3_x[i]
@@ -121,7 +115,8 @@ def angle_plot(count) :
         
         angle=np.arccos(cos_angle)
         angle2=angle*360/2/np.pi
-        hand_angle.append(angle2)       
+        hand_angle.append(angle2)
+"""     
     
 for file in files: 
     if not os.path.isdir(file): 
@@ -131,167 +126,123 @@ for file in files:
             #計算共有幾個人
             json_size= len(jf['people'])  
             # main_role position_x
-            if json_size!= 0:
-               
-             
-                if count==0 :  #第一筆資料，找最接近中心
-                  
-                    json_people= ghost(json_size, jf, main_role_x)  
-                  
-                    json_finger= ghost2(json_size, jf, main_role_x)  
-                  
-                    #print(json_people)
-                    #print(json_finger)
-                    #json_f= np.asarray(json_f)
-                  
-                    #json_finger= np.asarray(json_finger)                  
-                  
-                    #print(json_pre1)
+            if json_size!= 0:             
+                if count==0 :  #第一筆資料，找最接近中心  
+                    json_people= ghost(json_size, jf, main_role_x)
+                    json_finger= ghost2(json_size, jf, main_role_x)
+                    
+                    print(len(json_people))
+                    print(len(json_finger))
+                 
                     main_role_x=  json_people[1*3] 
                   
-                    body_0_y.append(frame_height-json_people[1+3*0])
+                    body[0]['y'].append(frame_height-json_people[1+3*0])
                   
-                    body_2_y.append(frame_height-json_people[1+3*2])
+                    body[2]['y'].append(frame_height-json_people[1+3*2])
                   
-                    body_2_x.append(json_people[0+3*2])
+                    body[2]['x'].append(json_people[0+3*2])
                   
-                    body_3_y.append(frame_height-json_people[1+3*3])
+                    body[3]['y'].append(frame_height-json_people[1+3*3])
                   
-                    body_3_x.append(json_people[0+3*3])
-        
-                    body_4_y.append(frame_height-json_people[1+3*4])
+                    body[3]['x'].append(json_people[0+3*3])
                   
-                    body_4_x.append(json_people[0+3*4])
-         
-                    body_6_y.append(frame_height-json_people[1+3*6])  
+                    body[4]['y'].append(frame_height-json_people[1+3*4])
                   
-                    body_10_y.append(frame_height-json_people[1+3*10])  # Lknee
+                    body[4]['x'].append(json_people[0+3*4])
                   
-                    body_13_y.append(frame_height-json_people[1+3*13])  # Rknee
+                    body[6]['y'].append(frame_height-json_people[1+3*6])
                   
-                    body_20_x.append(json_people[0+3*20])
+                    body[10]['y'].append(frame_height-json_people[1+3*10])
                   
-                    body_23_x.append(json_people[1+3*23])
-                 
-                    json_pre1= json_people
-                    json_pre2= json_people
-                    #print(main_role_x) 
+                    body[13]['y'].append(frame_height-json_people[1+3*13])
                   
-                    finger_12_x.append(json_finger[1+3*12])
-                    finger_12_y.append(json_finger[3*12])
+                    body[20]['x'].append(json_people[0+3*20])
                   
-                    count+= 1                 
-                                            
+                    body[23]['x'].append(json_people[0+3*2])                
+                  
+                    finger[12]['x'].append(json_finger[1+3*12]) 
+                  
+                    finger[12]['y'].append(json_finger[3*12]) 
+                  
+                    count+= 1                      
+                  
                 else : #其他找最靠近main_role
-               
                     json_people= ghost(json_size, jf, main_role_x)  
+                    json_finger= ghost2(json_size, jf, main_role_x)   
                   
-                    json_finger= ghost2(json_size, jf, main_role_x)  
+                    body[0]['y'].append(frame_height-json_people[1+3*0])
+                    
+                    body[2]['y'].append(frame_height-json_people[1+3*2])
+                    
+                    body[2]['x'].append(json_people[0+3*2])
+                    
+                    body[3]['y'].append(frame_height-json_people[1+3*3])
+                    
+                    body[3]['x'].append(json_people[0+3*3])
+                    
+                    body[4]['y'].append(frame_height-json_people[1+3*4])
+                    
+                    body[4]['x'].append(json_people[0+3*4])
+                    
+                    body[6]['y'].append(frame_height-json_people[1+3*6])
+                    
+                    body[10]['y'].append(frame_height-json_people[1+3*10])
+                    
+                    body[13]['y'].append(frame_height-json_people[1+3*13])
+                    
+                    body[20]['x'].append(json_people[0+3*20])
+                    
+                    body[23]['x'].append(json_people[0+3*2])
                   
-                    #print(json_people)
-                    #print(json_finger)
-                                  
-                    body_0_y.append(frame_height-json_people[1+3*0])
+                    #finger_12_x.append(json_finger[1+3*12])
+                    #finger_12_y.append(json_finger[3*12])
                   
-                    body_2_y.append(frame_height-json_people[1+3*2])
+                    finger[12]['x'].append(json_finger[1+3*12]) 
+                    finger[12]['y'].append(json_finger[3*12]) 
                   
-                    body_2_x.append(json_people[0+3*2])
-                  
-                    body_3_y.append(frame_height-json_people[1+3*3])
-                  
-                    body_3_x.append(json_people[0+3*3])
-                  
-                    body_4_y.append(frame_height-json_people[1+3*4])
-                  
-                    body_4_x.append(json_people[0+3*4])
+                    main_role_x=  (main_role_x+ json_people[1*3] )/2
          
-                    body_6_y.append(frame_height-json_people[1+3*6])
-                  
-                    body_10_y.append(frame_height-json_people[1+3*10])
-                  
-                    body_13_y.append(frame_height-json_people[1+3*13])
-                  
-                    body_20_x.append(json_people[0+3*20])
-                  
-                    body_23_x.append(json_people[1+3*23])
-                  
-                    json_pre2= json_pre1
-                    json_pre1= json_people
-                  
-                    finger_12_x.append(json_finger[1+3*12])
-                    finger_12_y.append(json_finger[3*12])
-                                                                    
-                    #print(json_pre1)
-                    #print(json_pre2)
-                    #print(jf['people'][temp]['pose_keypoints_2d'][1*3])
-                  
-                    main_role_x=  (main_role_x+ json_people[1*3])/2                  
+                    count+=1       
          
-                    count+=1     
-                           
-                frame_count.append(count)
-                #for i in range(0, 24):
-                    #x.append(json_f[3*i])
+           
           
-                #for i in range(0, 24):
-                    #y.append(json_f[1+ 3*i])    
+        frame_count.append(count)
+              #for i in range(0, 24):
+                #x.append(json_f[3*i])
+          
+              #for i in range(0, 24):
+                #y.append(json_f[1+ 3*i])    
+
+"""補點"""
+body= fill_lost(body)
+finger= fill_lost_finger(finger)
 
 
-#judge={"judge1":["angle:",judge_angle_1, "judge:",judge_standard_1, "comment:", judge_comment_1 ] , 
-#       "judge2":["angle:",judge_angle_2, "judge:",judge_standard_2, "comment:", judge_comment_2], 
-#       "judge3":["judge:",judge_3_flag ]}
-#file_name = 'judge.json' #指定檔案儲存的資料為json格式
-
-#with open(file_name,'w') as file_object:
-#     json.dump(judge,file_object)
-
-#angle_plot(count)
-
-body_0_x= fill_lose(body_0_x)
-body_2_x= fill_lose(body_2_x)
-body_3_x= fill_lose(body_3_x)
-body_4_x= fill_lose(body_4_x)   
-body_6_x= fill_lose(body_6_x)  
-body_10_x= fill_lose(body_10_x)  
-body_13_x= fill_lose(body_13_x)
-body_20_x= fill_lose(body_20_x)
-body_23_x= fill_lose(body_23_x)
-
-body_0_y= fill_lose(body_0_y)
-body_2_y= fill_lose(body_2_y)
-body_3_y= fill_lose(body_3_y)
-body_4_y= fill_lose(body_4_y)   
-body_6_y= fill_lose(body_6_y)  
-body_10_y= fill_lose(body_10_y)  
-body_13_y= fill_lose(body_13_y)
-
-finger_12_x= fill_lose(finger_12_x)
-finger_12_y= fill_lose(finger_12_y)
-
+"""
 #print(len(body_0_x))
 #print(body_0_x)
 #if len(body_0_x)==0 or len(body_2_x) or len(body_3_x) or len(body_4_x) or len(body_6_x) or len(body_10_x) or len(body_13_x) or len(body_20_x) or len(body_23_x) :  
     #error= True 
 #print(error)   
 
-"""
+
 knee_lowest= knee_low(body_10_y,count)
 print(knee_lowest)  
 judge_angle_1, judge_standard_1, judge_comment_1=shoot_judge1(knee_lowest, judge_standard_1, body_2_x, body_3_x, body_4_x,
                   body_2_y, body_3_y, body_4_y)
 """
 
-el_sh= elbow_shoulder(body_2_y, body_3_y, count)
+el_sh= elbow_shoulder(body[2]['y'], body[3]['y'], count)
 print(el_sh)
-judge_angle_1, judge_standard_1, judge_comment_1=shoot_judge1(el_sh, judge_standard_1, body_2_x, body_3_x, body_4_x,
-                  body_2_y, body_3_y, body_4_y)      
+judge_angle_1, judge_standard_1, judge_comment_1=shoot_judge1(el_sh, judge_standard_1, body[2]['x'], body[3]['x'], body[4]['x'],
+                  body[2]['y'], body[3]['y'], body[4]['y'])      
 
-hand_highest= hand_high(body_2_y, count)
+hand_highest= hand_high(body[2]['y'], count)
 print(hand_highest)       
-judge_angle_2, judge_standard_2, judge_comment_2=shoot_judge2(hand_highest,judge_standard_2, body_2_x, body_3_x, body_4_x,
-                  body_2_y, body_3_y, body_4_y)  
+judge_angle_2, judge_standard_2, judge_comment_2=shoot_judge2(hand_highest,judge_standard_2, body[2]['x'], body[3]['x'], body[4]['x'],
+                  body[2]['y'], body[3]['y'], body[4]['y'])  
 
-judge3= shoot_judge3(hand_highest, finger_12_x, finger_12_y, body_3_x, body_4_x, body_3_y, body_4_y)
+judge3= shoot_judge3(hand_highest, finger[12]['x'], finger[12]['y'], body[3]['x'], body[4]['x'], body[3]['y'], body[4]['y'])
 print(judge3)
 
 # print(type(video_id))
